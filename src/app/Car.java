cbe7b308726ae188ba5a49adbaf7b93cdaaf4d04
ahmadhdr <ahmadhdr.22@gmail.com>
2020-05-02 22:06:37 +0700

    basic class

---
+++ src/app/Car.java
@@ -0,0 +1,20 @@
+package app;
+
+public class Car {
+  public String wheels;
+  public String model;
+  private String engine;
+  private String colour;
+  private int dors;
+
+  public static void main(String[] args) {
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public String getModel() {
+    return this.model;
+  }
+}
---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+  public static void main(String[] args) {
+    Car porsche = new Car();
+    Car holder  = new Car();
+
+    holder.setModel("MANTAP");
+    System.out.println("the model is: " + holder.getModel());
+  }
+}
